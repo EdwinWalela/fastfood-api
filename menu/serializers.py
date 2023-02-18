9527c32d8ff9eb8ec13cab3cf691e2666ce49890
edwinwalela <edwinwalela@gmail.com>
2023-02-18 17:56:25 +0300

    include category in menu item list

--- menu/serializers.py
+++ menu/serializers.py
@@ -1,13 +1,23 @@
 from rest_framework import serializers
 from .models import Category, MenuItem
 
+
+class CategoryRelatedField(serializers.RelatedField):
+  def to_representation(self, value):
+    return str(value)
+  def to_internal_value(self, data):
+    return Category.objects.get(pk=data)
+
 class CategorySerializer(serializers.ModelSerializer):
   class Meta:
     model = Category
     fields = ['id','title']
     
+  
 class MenuItemSerializer(serializers.ModelSerializer):
-  category = serializers.StringRelatedField()
+  category = CategoryRelatedField(
+    queryset=Category.objects.all
+  )
   class Meta:
     model = MenuItem
     fields = ['id','title','description','price','category']
