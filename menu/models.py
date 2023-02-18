e450ebc8187f58b5c9b85c2dd0bbc9658508b2aa
edwinwalela <edwinwalela@gmail.com>
2023-02-18 12:47:33 +0300

    create category migration

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+  class Meta:
+    verbose_name_plural = 'categories'
+  def __str__(self) -> str:
+    return self.title
+  title = models.CharField(max_length=200)  
