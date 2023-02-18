f88221cb6b41ee32cc8817b5524cf3ee6d9b7924
edwinwalela <edwinwalela@gmail.com>
2023-02-18 14:22:04 +0300

    convert to class based view

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from rest_framework.urlpatterns import format_suffix_patterns
 from django.urls import path
 from menu import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('categories',views.category_list)
+    path('categories',views.CategoryList.as_view())
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- menu/views.py
+++ menu/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from .models import Category
 from .serializers import CategorySerializer
 
-@api_view(['GET'])
-def category_list(request):
-  categories = Category.objects.all()
-  serializer = CategorySerializer(categories,many=True)
-  return JsonResponse({'categories':serializer.data})
+class CategoryList(APIView):
+  """
+  List all categories or create a new category
+  """
+  def get(self,request,format=None):
+    categories = Category.objects.all()
+    serializer = CategorySerializer(categories,many=True)
+    return JsonResponse({'categories':serializer.data})
+  
+  def post(self,request,format=None):
+    serializer = CategorySerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return JsonResponse({
+        'message':'category created',
+        'category':serializer.data
+      })
+    return JsonResponse({
+        'message':'failed to create category',
+        'errors':serializer.errors
+    })
